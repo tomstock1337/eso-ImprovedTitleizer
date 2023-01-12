4fef055e9b2211270265a66a98535335a18f096e
MSI\thoma <thomas.stockwell@gmail.com>
2023-01-12 17:14:25 -0500

    More categorization adjustments.

--- ImprovedTitleizer.lua
+++ ImprovedTitleizer.lua
@@ -40,62 +40,144 @@ local AchievmentIdsCategories =
 		Name="Alliance War",
 		Entries=
 		{
-			{ID=92,Rank=1}, --Volunteer
-			{ID=93,Rank=2}, --Recruit
-			{ID=94,Rank=3}, --Tyro
-			{ID=95,Rank=4}, --Legionary
-			{ID=96,Rank=5}, --Veteran
-			{ID=97,Rank=6}, --Corporal
-			{ID=98,Rank=7}, --Sergeant
-			{ID=706,Rank=8}, --First Sergeant
-			{ID=99,Rank=9}, --Lieutenant
-			{ID=100,Rank=10}, --Captain
-			{ID=101,Rank=11}, --Major
-			{ID=102,Rank=12}, --Centurion
-			{ID=103,Rank=13}, --Colonel
-			{ID=104,Rank=14}, --Tribune
-			{ID=105,Rank=15}, --Brigadier
-			{ID=106,Rank=16}, --Prefect
-			{ID=107,Rank=17}, --Praetorian
-			{ID=108,Rank=18}, --Palatine
-			{ID=109,Rank=19}, --August Palatine
-			{ID=110,Rank=20}, --Legate
-			{ID=111,Rank=21}, --General
-			{ID=112,Rank=22}, --Warlord
-			{ID=113,Rank=23}, --Grand Warlord
-			{ID=114,Rank=24}, --Overlord
-			{ID=705,Rank=25}, --Grand Overlord
+			{ID=92,Rank=1}, --Volunteer --Alliance War Volunteer
+			{ID=93,Rank=2}, --Recruit --Alliance War Recruit
+			{ID=94,Rank=3}, --Tyro --Alliance War Tyro
+			{ID=95,Rank=4}, --Legionary --Alliance War Legionary
+			{ID=96,Rank=5}, --Veteran --Alliance War Veteran
+			{ID=97,Rank=6}, --Corporal --Alliance War Corporal
+			{ID=98,Rank=7}, --Sergeant --Alliance War Sergeant
+			{ID=706,Rank=8}, --First Sergeant --Alliance War First Sergeant
+			{ID=99,Rank=9}, --Lieutenant --Alliance War Lieutenant
+			{ID=100,Rank=10}, --Captain --Alliance War Captain
+			{ID=101,Rank=11}, --Major --Alliance War Major
+			{ID=102,Rank=12}, --Centurion --Alliance War Centurion
+			{ID=103,Rank=13}, --Colonel --Alliance War Colonel
+			{ID=104,Rank=14}, --Tribune --Alliance War Tribune
+			{ID=105,Rank=15}, --Brigadier --Alliance War Brigadier
+			{ID=106,Rank=16}, --Prefect --Alliance War Prefect
+			{ID=107,Rank=17}, --Praetorian --Alliance War Praetorian
+			{ID=108,Rank=18}, --Palatine --Alliance War Palatine
+			{ID=109,Rank=19}, --August Palatine --Alliance War August Palatine
+			{ID=110,Rank=20}, --Legate --Alliance War Legate
+			{ID=111,Rank=21}, --General --Alliance War General
+			{ID=112,Rank=22}, --Warlord --Alliance War Warlord
+			{ID=113,Rank=23}, --Grand Warlord --Alliance War Grand Warlord
+			{ID=114,Rank=24}, --Overlord --Alliance War Overlord
+			{ID=705,Rank=25}, --Grand Overlord --Alliance War Grand Overlord
 		},
+	},
+	{
+		Name="Arena",
+		Entries=
+		{
+			{ID=992}, --Dragonstar Arena Champion --Dragonstar Arena Champion
+			{ID=1140}, --Boethiah's Scythe --Dragonstar Arena Conqueror
+			{ID=1304}, --Maelstrom Arena Champion --Maelstrom Arena Champion
+			{ID=1305}, --Stormproof --Maelstrom Arena Conqueror
+			{ID=1330}, --The Flawless Conqueror --Maelstrom Arena: Perfect Run
+			{ID=2362}, --Blackrose Condemner --Blackrose Prison Vanquisher
+			{ID=2363}, --Blackrose Executioner --Blackrose Prison Conqueror
+			{ID=2368}, --The Unchained --God of the Gauntlet
+			{ID=2908}, --Spiritblood Champion --Vateshran Hollows Conqueror
+			{ID=2912}, --Spirit Slayer --The Vateshran's Chosen
+			{ID=2913}, --of the Undying Song --Hero of the Unending Song
+		}
 	},
 	{
 		Name="Battlegrounds",
 		Entries=
 		{
-			{ID=1918}, -- [Paragon] Paragon
-			{ID=1915}, -- [Battleground Butcher] Battleground Butcher
-			{ID=1913}, -- [Grand Champion] Grand Champion
-			{ID=1916}, -- [Tactician] Tactician
-			{ID=1919}, -- [Triple Threat] Relic Runner
-			{ID=1910}, -- [Conquering Hero] Conquering Hero
-			{ID=1895}, -- [Pit Hero] Bloodletter
-			{ID=1901}, -- [Grand Relic Guardian] Relic Guardian
-			{ID=1898}, -- [Grand Relic Hunter] Relic Hunter
-			{ID=1904}, -- [Grand Standard-Bearer] Standard-Bearer
-			{ID=1907}, -- [Grand Standard-Guardian] Standard-Guardian
-			{ID=1921}, -- [Quadruple Kill] The Merciless
+			{ID=1895}, --Bloodletter --Pit Hero
+			{ID=1898}, --Relic Hunter --Grand Relic Hunter
+			{ID=1901}, --Relic Guardian --Grand Relic Guardian
+			{ID=1904}, --Standard-Bearer --Grand Standard-Bearer
+			{ID=1910}, --Conquering Hero --Conquering Hero
+			{ID=1913}, --Grand Champion --Grand Champion
+			{ID=1919}, --Relic Runner --Triple Threat
+			{ID=1954}, --Chaos Guardian --Chaos Guardian
+			{ID=1955}, --Chaos Champion --Chaos Champion
+
+		}
+	},
+	{
+		Name="Dungeon",
+		Entries=
+		{
+			{ID=1159}, --Deadlands Adept --Deadlands Savvy
+			{ID=1538}, --Hist-Shadow --Shadows of the Hist Delver
+			{ID=1691}, --Bane of Beastmen --Bloodroot Forge Conqueror
+			{ID=1696}, --Forge Breaker --Tempered Tantrum
+			{ID=1699}, --Jarl Maker --Falkreath Hold Conqueror
+			{ID=1704}, --Thane of Falkreath --Taking the Bull by the Horns
+			{ID=1960}, --Blackmarrow's Bane --Fang Lair Conqueror
+			{ID=1965}, --Dovahkriid --Let Bygones Be Bygones
+			{ID=1976}, --Peak Scaler --Scalecaller Peak Conqueror
+			{ID=1981}, --Plague of Peryite --Breaker of Spells
+			{ID=2153}, --Silver Knight --Moon Hunter Keep Conqueror
+			{ID=2154}, --Alpha Predator --The Alpha Predator
+			{ID=2163}, --Huntmaster --March of Sacrifices Conqueror
+			{ID=2164}, --Hircine's Champion --Hircine's Champion
+			{ID=2266}, --Frozen Treasure Seeker --Frostvault Challenger
+			{ID=2275}, --Purified Devastator --Depths of Malatar Challenger
+			{ID=2421}, --Chevalier --Moongrave Fane Challenger
+			{ID=2427}, --Z'en's Redeemer --Selene's Savior
+			{ID=2430}, --Guardian of the Green --Lair of Maarselok Challenger
+			{ID=2541}, --Witch Hunter --Cold-Blooded Killer
+			{ID=2546}, --Storm Foe --No Rest for the Wicked
+			{ID=2551}, --Sanctifier --Skull Smasher
+			{ID=2555}, --Bonecaller's Bane --In Defiance of Death
+			{ID=2701}, --True Genius --True Genius
+			{ID=2706}, --The Inedible --Thorn Remover
+			{ID=2710}, --Bane of Thorns --Bane of Thorns
+			{ID=2751}, --Dark Delver --Companion of Lyris Titanborn
+			{ID=2755}, --Pinnacle of Evolution --Triple Checked
+			{ID=2833}, --Flamechaser --Snuffed Out
+			{ID=2838}, --Ardent Bibliophile --Ardent Bibliophile
+			{ID=2843}, --Spark of Vengeance --Schemes Disrupted
+			{ID=2847}, --Subterranean Smasher --Subterranean Smasher
+			{ID=3018}, --Seeker of Artifacts --Prior Offenses
+			{ID=3023}, --of the Silver Rose --Bastion Breaker
+			{ID=3028}, --Incarnate --Unshaken
+			{ID=3032}, --The Dreaded --Battlespire's Best
+			{ID=3111}, --Coral Caretaker --Land, Air, and Sea Supremacy
+			{ID=3120}, --Privateer --Zero Regrets
+			{ID=3153}, --Gryphon Handler --Superior Pedigree
+			{ID=3154}, --Tide Turner --Shove Off
+			{ID=3224}, --Shipwright --Sans Spirit Support
+			{ID=3226}, --Aerie Ascender --Tentacless Triumph
+			{ID=3377}, --Earthen Root Avenger --Earthen Root Avenger
+			{ID=3381}, --Invaders' Bane --Invaders' Bane
+			{ID=3396}, --Breathless --Breathless
+			{ID=3400}, --Fist of Tava --Fist of Tava
+			{ID=3525}, --Earthen Root Champion --Earthen Root Enclave Champion
+			{ID=3526}, --Graven Deep Champion --Graven Deep Champion
+		}
+	},
+	{
+		Name="Holiday",
+		Entries={
+			{ID=1546}, --Sun's Dusk Reaper --An Unsparing Harvest
+			{ID=1677}, --Magnanimous --Glory of Magnus
+			{ID=1716}, --Lord of Misrule --<<player{Lord/Lady}>> of Misrule
+			{ID=1723}, --Royal Jester --Royal Jester
+			{ID=1892}, --Star-Made Knight --Star-Made Knight
+			{ID=2453}, --Tin Soldier --The Upper Crust
+			{ID=2458}, --Empieror --Messy Business
+			{ID=2587}, --Witch --Wicked Writ Witch
 		}
 	},
 	{
 		Name="Tales of Tribute",
 		Entries={
-			{ID=3349, Rank=1}, -- Roister's Club Initiate
-			{ID=3350, Rank=2}, -- Roister's Club Trainee
-			{ID=3351, Rank=3}, -- Roister's Club Novice
-			{ID=3352, Rank=4}, -- Roister's Club Regular
-			{ID=3353, Rank=5}, -- Roister's Club Adept
-			{ID=3354, Rank=6}, -- Roister's Club Expert
-			{ID=3355, Rank=7}, -- Roister's Club Veteran
-			{ID=3356, Rank=8}, -- Roister's Club Master
+			{ID=3349,Rank=1}, --Roister's Club Initiate --Roister's Club Initiate
+			{ID=3350,Rank=2}, --Roister's Club Trainee --Roister's Club Trainee
+			{ID=3351,Rank=3}, --Roister's Club Novice --Roister's Club Novice
+			{ID=3352,Rank=4}, --Roister's Club Regular --Roister's Club Regular
+			{ID=3353,Rank=5}, --Roister's Club Adept --Roister's Club Adept
+			{ID=3354,Rank=6}, --Roister's Club Expert --Roister's Club Expert
+			{ID=3355,Rank=7}, --Roister's Club Veteran --Roister's Club Veteran
+			{ID=3356,Rank=8}, --Roister's Club Master --Roister's Club Master
 		}
 	},
 	{
@@ -109,58 +191,16 @@ local AchievmentIdsCategories =
 		}
 	},
 	{
-		Name="Conqueror",
-		Entries={
-			{ID=1330}, --: The Flawless Conqueror -- Maelstrom Arena: Perfect Run ",
-			{ID=1305}, --: Stormproof -- Maelstrom Arena Conqueror ",
-			{ID=1699}, -- Jarl Maker -- Falkreath Hold Conqueror ",
-			{ID=1691}, -- Bane of Beastmen -- Bloodroot Forge Conqueror ",
-			{ID=2077}, -- Assistant Alienist -- Asylum Sanctorium Conqueror ",
-			{ID=1960}, -- Blackmarrow's Bane -- Fang Lair Conqueror ",
-			{ID=1976}, -- Peak Scaler -- Scalecaller Peak Conqueror ",
-			{ID=2163}, -- Huntmaster -- March of Sacrifices Conqueror ",
-			{ID=2153}, -- Silver Knight -- Moon Hunter Keep Conqueror ",
-			{ID=2363}, -- Blackrose Executioner -- Blackrose Prison Conqueror ",
-			{ID=2908}, -- Spiritblood Champion -- Vateshran Hollows Conqueror ",
-			{ID=1810}, -- Divayth Fyr's Coadjutor -- Halls of Fabrication Conqueror ",
-			{ID=2133}, -- Shadow Breaker -- Cloudrest Conqueror ",
-			{ID=2435}, -- Sunspire Saint -- Sunspire Conqueror ",
-			{ID=2734}, -- Kyne's Will -- Kyne's Aegis Conqueror ",
-			{ID=2987}, -- Ca-Uxith Warrior -- Rockgrove Conqueror ",
-			{ID=3244}, -- Seaborne Slayer -- Dreadsail Reef Conqueror ",
-			{ID=1474}, -- Shehai Shatterer -- Hel Ra Citadel Conqueror ",
-			{ID=1503}, -- Mageslayer -- Aetherian Archive Conqueror ",
-			{ID=1462}, -- Ophidian Overlord -- Sanctum Ophidia Conqueror ",
-			{ID=1140}, -- Boethiah's Scythe -- Dragonstar Arena Conqueror ",
-		}
-	},
-	{
 		Name="Savior",
 		Entries={
-			{ID=2941}, -- Guardian of the Reach -- Savior of the Reach ",
-			{ID=3501}, -- Guardian of Galen -- Savior of Galen ",
-			{ID=1868}, -- Savior of Morrowind -- Savior of Morrowind ",
-			{ID=2193}, -- Savior of Summerset -- Savior of Summerset ",
-			{ID=2509}, -- Savior of Elsweyr -- Savior of Elsweyr ",
-			{ID=2712}, -- Savior of Western Skyrim -- Savior of Western Skyrim ",
-			{ID=3047}, -- Savior of Blackwood -- Savior of Blackwood ",
-			{ID=3271}, -- Savior of High Isle -- Savior of High Isle ",
-			{ID=587}, -- Savior of Nirn -- Anchors Away ",
-		}
-	},
-	{
-		Name="Hero",
-		Entries={
-			{ID=2623}, -- Hero of the Dragonguard -- Hero of the Dragonguard ",
-			{ID=2913}, -- of the Undying Song -- Hero of the Unending Song ",
-			{ID=618}, -- Dominion Hero -- Hero of the Aldmeri Dominion ",
-			{ID=61}, -- Covenant Hero -- Hero of the Daggerfall Covenant ",
-			{ID=617}, -- Pact Hero -- Hero of the Ebonheart Pact ",
-			{ID=1248},-- Hero of Wrothgar -- Hero of Wrothgar ",
-			{ID=2049},-- Hero of Clockwork City -- Hero of Clockwork City ",
-			{ID=2331},-- Hero of Murkmire -- Hero of Murkmire ",
-			{ID=2939},-- Hero of Skyrim -- A Bridge Between Kingdoms ",
-			{ID=3145},-- Hero of Fargrave -- Hero of Fargrave ",
+			{ID=587}, --Savior of Nirn --Anchors Away
+			{ID=1868}, --Savior of Morrowind --Savior of Morrowind
+			{ID=2193}, --Savior of Summerset --Savior of Summerset
+			{ID=2509}, --Savior of Elsweyr --Savior of Elsweyr
+			{ID=2712}, --Savior of Western Skyrim --Savior of Western Skyrim
+			{ID=3047}, --Savior of Blackwood --Savior of Blackwood
+			{ID=3271}, --Savior of High Isle --Savior of High Isle
+			{ID=3512}, --Sower's Savior --Firesong Extinguisher
 		}
 	},
 	{
@@ -174,15 +214,47 @@ local AchievmentIdsCategories =
 		}
 	},
 	{
-		Name="Trial Trifectas",
+		Name="Trials",
 		Entries={
-			{ID=2087},-- Saintly Savior -- Perfect Purification
-			{ID=1838},-- Tick-Tock Tormentor -- Like Clockwork
-			{ID=2139},-- Gryphon Heart -- The Path to Alaxon
-			{ID=2467},-- Godslayer -- Godslayer of Sunspire
-			{ID=2740},-- Kyne's Wrath -- Stainless Siege-breaker
-			{ID=3003},-- Planesbreaker -- Soul Savior
-			{ID=3248},-- Soul of the Squall -- Fleet Queen's Foil
+			{ID=1391}, --Dro-m'Athra Destroyer --Maw of Lorkhaj: Moons' Champion
+			{ID=1462}, --Ophidian Overlord --Sanctum Ophidia Conqueror
+			{ID=1474}, --Shehai Shatterer --Hel Ra Citadel Conqueror
+			{ID=1503}, --Mageslayer --Aetherian Archive Conqueror
+			{ID=1808}, --Clockwork Confounder --Halls of Fabrication Completed
+			{ID=1810}, --Divayth Fyr's Coadjutor --Halls of Fabrication Conqueror
+			{ID=1836}, --The Dynamo --Dynamo
+			{ID=1837}, --Disassembly General --Stress Tested
+			{ID=1838}, --Tick-Tock Tormentor --Like Clockwork
+			{ID=2075}, --Immortal Redeemer --Asylum Sanctorium Redeemer
+			{ID=2076}, --Orderly --Asylum Sanctorium Completed
+			{ID=2077}, --Assistant Alienist --Asylum Sanctorium Conqueror
+			{ID=2079}, --Voice of Reason --Asylum Sanctorium Vanquisher
+			{ID=2087}, --Saintly Savior --Perfect Purification
+			{ID=2131}, --Cloudrest Hero --Cloudrest Completed
+			{ID=2133}, --Shadow Breaker --Cloudrest Conqueror
+			{ID=2136}, --Bringer of Light --Cloudrest Vanquisher
+			{ID=2139}, --Gryphon Heart --The Path to Alaxon
+			{ID=2140}, --Welkynar Liberator --Cloudrest Savior
+			{ID=2433}, --Sunspire Ascendant --Sunspire Completed
+			{ID=2435}, --Sunspire Saint --Sunspire Conqueror
+			{ID=2466}, --Extinguisher of Flames --Sunspire Vanquisher
+			{ID=2467}, --Godslayer --Godslayer of Sunspire
+			{ID=2468}, --Hand of Alkosh --Sunspire Dragonbreak
+			{ID=2732}, --Kyne's Chosen --Kyne's Aegis Completed
+			{ID=2734}, --Kyne's Will --Kyne's Aegis Conqueror
+			{ID=2739}, --Shield of the North --Kyne's Aegis Vanquisher
+			{ID=2740}, --Kyne's Wrath --Stainless Siege-breaker
+			{ID=2746}, --Dawnbringer --Kyne's Deliverance
+			{ID=2985}, --Defender of Rockgrove --Rockgrove Completed
+			{ID=2987}, --Ca-Uxith Warrior --Rockgrove Conqueror
+			{ID=3003}, --Planesbreaker --Soul Savior
+			{ID=3004}, --Daedric Bane --Xalvakka's Bane
+			{ID=3007}, --Xalvakka's Scourge --Rockgrove Vanquisher
+			{ID=3242}, --Dreadsails' Scourge --Dreadsail Reef Vanquisher
+			{ID=3244}, --Seaborne Slayer --Dreadsail Reef Conqueror
+			{ID=3248}, --Soul of the Squall --Fleet Queen's Foil
+			{ID=3249}, --Swashbuckler Supreme --Swashbuckler Supreme
+			{ID=3252}, --Hurricane Herald --Master Marine
 		}
 	}
 }
@@ -203,8 +275,11 @@ local GetNextAchievementInLine           = GetNextAchievementInLine
 local GetAchievementSubCategoryInfo      = GetAchievementSubCategoryInfo
 --Usage: local subCategoryName, subNumAchievements = GetAchievementSubCategoryInfo(categoryIndex, subCategoryIndex)
 local GetAchievementId                   = GetAchievementId
+--Usage: local achievementId = GetAchievementId(categoryIndex, subcategoryIndex, achievementIndex)
+local GetAchievementInfo                 = GetAchievementInfo
+--Usage local name, description, points, icon, completed, date, time = GetAchievementInfo(achievementId)
 
---Usage: achievementId = GetAchievementId(categoryIndex, subcategoryIndex, achievementIndex)
+
 local GetTitle                           = GetTitle -- I want original titles
 --Usage: local strTitle GetTitle(achievementId)
 
@@ -219,6 +294,7 @@ local function InitializeTitles()
 			local hasTitle, title = GetAchievementRewardTitle(id)
 			if hasTitle then
 				local achieveName = GetAchievementNameFromLink(GetAchievementLink(id))
+				local name, description, points, icon, completed, date, time = GetAchievementInfo(id)
 				local playerHasTitle = false
 				local playerTitleIndex = -1
 				for j=1,GetNumTitles() do
@@ -228,7 +304,7 @@ local function InitializeTitles()
 						break
 					end
 				end
-				table.insert(AllTitles,1,{AchievementID=id,TitleID=playerTitleIndex,Title=title,CategoryID=categoryId, CategoryName=categoryName,SubCategoryID=subCategory,SubCategoryName=subCategoryName,HasTitle=playerHasTitle,AchievementName=achieveName},1);
+				table.insert(AllTitles,1,{AchievementID=id,TitleID=playerTitleIndex,Title=title,CategoryID=categoryId, CategoryName=categoryName,SubCategoryID=subCategory,SubCategoryName=subCategoryName,HasTitle=playerHasTitle,AchievementName=achieveName,AchievementDescription=description},1);
 			end
 			id = GetNextAchievementInLine(id)
 		end
@@ -362,7 +438,9 @@ SLASH_COMMANDS["/dumptitles"] = function()
 		output = output.."Title: "..vTitle.Title.." "
 		output = output.."CategoryID: "..vTitle.CategoryID.." "
 		output = output.."CategoryName: "..vTitle.CategoryName.." "
+		output = output.."AchievementID: "..vTitle.AchievementID.." "
 		output = output.."AchievementName: "..vTitle.AchievementName.." "
+		output = output.."AchievementDescription: "..vTitle.AchievementDescription.." "
 		d(output)
 	end
 end

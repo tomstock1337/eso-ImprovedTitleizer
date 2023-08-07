9c73dfa87c616eeb8c4ce1798ca9a10aecfd1c27
MSI\thoma <thomas.stockwell@gmail.com>
2023-08-07 16:41:14 -0400

    Version increment

--- ImprovedTitleizer/ImprovedTitleizer.lua
+++ ImprovedTitleizer/ImprovedTitleizer.lua
@@ -7,7 +7,7 @@ local ImprovedTitleizer = {}
 ImprovedTitleizer.Name = "ImprovedTitleizer"
 ImprovedTitleizer.DisplayName = "ImprovedTitleizer"
 ImprovedTitleizer.Author = "tomstock"
-ImprovedTitleizer.Version = "1.3.2"
+ImprovedTitleizer.Version = "1.4"
 ImprovedTitleizer.Debug = false
 --[[
 	==============================================
